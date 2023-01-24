a504128fc3da2ddcecdaa7dbe6ad040e80d24477
Ashish <phularaashish12345@gmail.com>
2023-01-25 00:17:28 +0530

    added functinality for adding and removing users

--- controllers/getUsers.js
+++ controllers/getUsers.js
@@ -27,3 +27,39 @@ export const  getUserFriends = async (req,res)=>{
     res.status(403).json({message:err.message})
 }
 }
+
+// update a user
+
+const addRemoveFriend = async (req,res)=>{
+    try{
+        const {id ,friendId}= req.body; // we are getting the user id and friend id
+        const user = await user.findById(id); // finding the user
+
+        const  friend = await user.findById(friendId); // finding the friend
+
+        if(user.friends.includes(friendId)){
+            user.friends = user.friends.filter((id)=>(id)!= friendId)  // we are removing the friend
+            friend.friends = friend.friends.filter((id)=>(id)!=id)  //  we are removing the friend    
+        }else{
+            user.friends.push(friendId)
+            friend.friends.push(id)
+        }
+        await user.save();
+        await friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map((id)=>user.findById(id))
+        )
+            const formatFriends = friends.map(
+                ({_id,firstName , lastName , occupation ,location, imagePath})=>{
+                    return {_id,firstName , lastName , occupation ,location, imagePath}
+                })
+                res.status(200).json(formatFriends)
+
+    }catch(err){
+        res.status(403).json({message:err.message})
+    }
+}
+
+
+
